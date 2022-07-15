ff07bc353544466d0bb2dd2a8184c58bf9454bf0
Thiago B. Rodrigues <tbarreto347@gmail.com>
2022-07-15 16:45:01 -0400

    Pages with ready links lacking only responsiveness

---
+++ assets/js/pages.js
@@ -0,0 +1,12 @@
+function production() {
+    window.location.href = "assets/pages/production.html"
+}
+function quality() {
+    window.location.href = "assets/pages/quality.html"
+}
+function repair() {
+    window.location.href = "assets/pages/repair.html"
+}
+function smt() {
+    window.location.href = "assets/pages/smt.html"
+}
